f71514588f1d39fe6bb23dd5f53a2e9633d6e908
super-duper-man <111101555+super-duper-man@users.noreply.github.com>
2024-08-10 09:12:32 +0330

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Link } from "@nextui-org/link";
+import { Snippet } from "@nextui-org/snippet";
+import { Code } from "@nextui-org/code";
+import { button as buttonStyles } from "@nextui-org/theme";
+
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon } from "@/components/icons";
+
+export default function Home() {
+  return <p className="text-green-700 text-center">Hello Auth</p>;
+}
---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Next-auth Sample!",
+  description:
+    "Next-auth Sample is a nestjs project to demonstrate how next-auth works in a real world project.",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+  ],
+};
